0fce56e9867e24681d43451896bea3feb98d4df3
Ivan Shulga <shg.ivan97@gmail.com>
2019-08-12 16:44:46 +0300

    gallery script

--- js/common.js
+++ js/common.js
@@ -1,3 +1,17 @@
 $(function(){
     $("#number").mask("+7 (999) 999-99-99", {placeholder: " "});
   });
+
+$('.diploms').on(`init reInit`, function(event, slick) {
+  $('.diploms__arrow').text('0' + 1 + '/' + '0' + slick.slideCount);
+})
+$('.diploms').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
+  $('.diploms__arrow').text('0' + (1 + currentSlide) + '/' + '0' + slick.slideCount);
+})
+$('.diploms').slick({
+  slidesToShow: 1,
+  dots: false,
+  appendArrows:'.diplom__arrow',
+  prevArrow: '<button class="pre-next pre"><img src="./img/right-arrow.png"/></button>',
+  nextArrow: '<button class="pre-next next"><img src="./img/right-arrow.png"/></button>'
+});
