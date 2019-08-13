f9fe8260ad1dc802d39af71449dce01a2c1188f7
Ivan Shulga <shg.ivan97@gmail.com>
2019-08-13 18:21:45 +0300

    add popup with script

--- js/common.js
+++ js/common.js
@@ -1,7 +1,9 @@
+//jQuery Mask
 $(function(){
     $("#number").mask("+7 (999) 999-99-99", {placeholder: " "});
   });
 
+//Gallery
 $('.diploms').on(`init reInit`, function(event, slick) {
   $('.diploms__arrow').text('0' + 1 + '/' + '0' + slick.slideCount);
 })
@@ -15,3 +17,22 @@ $('.diploms').slick({
   prevArrow: '<button class="pre-next pre"><img src="./img/right-arrow.png"/></button>',
   nextArrow: '<button class="pre-next next"><img src="./img/right-arrow.png"/></button>'
 });
+
+//Header Scroll
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 800) {
+      $('nav').addClass('fixed');
+  } else {
+      $('nav').removeClass('fixed');
+  }
+});
+
+//Popup
+$('.show_popup').click(function() {
+  var popup_id = $('#' + $(this).attr("rel"));
+  $(popup_id).show();
+  $('.overlay_popup').show();
+})
+$('.overlay_popup').click(function() {
+  $('.overlay_popup, .popup__form').hide();
+})
